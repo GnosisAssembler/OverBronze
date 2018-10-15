ebe27f99d94bbdb29a244157e719d0d77df4ec68
Panos Karypiadis <pan.karyp@gmail.com>
2018-10-15 15:37:26 +0300

    Add owner id, ready event, commands/general.js

--- bot.js
+++ bot.js
@@ -4,17 +4,15 @@ const path = require('path');
 
 // Init commando client - instance of client
 const client = new Commando.Client({
-    commandPrefix: '<Insert Your Prefix Here>',
-    owner: '<Insert Your User ID Here>',
+    commandPrefix: '~',
+    owner: '377149978902134794',
     disableEveryone: true
 });
 
 client.registry
     // Registers your custom command groups
     .registerGroups([
-        ['fun', 'Fun commands'],
-        ['some', 'Some group'],
-        ['other', 'Some other group']
+        ['basic', 'Basic commands']
     ])
 
     // Registers all built-in groups, commands, and argument types
@@ -23,6 +21,12 @@ client.registry
     // Registers all of your commands in the ./commands/ directory
     .registerCommandsIn(path.join(__dirname, 'commands'));
 
+// Set ready event
+client.on('ready', () => {
+    console.log('OverBronze bot logged in!');
+    client.user.setActivity('game');
+});
+
 
 const sqlite = require('sqlite');
 
@@ -30,4 +34,5 @@ client.setProvider(
     sqlite.open(path.join(__dirname, 'settings.sqlite3')).then(db => new Commando.SQLiteProvider(db))
 ).catch(console.error);
 
+// Login with bot token
 client.login(require('./config/keys.js').botToken);
